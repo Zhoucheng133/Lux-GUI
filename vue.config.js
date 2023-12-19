9e68176bc5431e09aa84b439e129d8ec040349d7
zhoucheng <zhoucheng133@outlook.com>
2023-12-19 11:36:58 +0800

    添加Electron

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      // 对于macOS，分别导出x64版本和Apple Silicon版本
+      builderOptions: {
+        artifactName: '${productName}-${version}-${os}-${arch}.${ext}',
+        mac: {
+          target: {
+            arch: ['x64', 'arm64'],
+            target: 'zip'
+          }
+        }
+      },
+    },
+  },
 })
