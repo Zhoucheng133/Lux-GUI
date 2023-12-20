2f927cc6655060dffb9d3302ef8968fead042b47
zhoucheng <zhoucheng133@outlook.com>
2023-12-20 09:26:01 +0800

    修改Electron配置

--- src/background.js
+++ src/background.js
@@ -1,5 +1,3 @@
-'use strict'
-
 import { app, protocol, BrowserWindow } from 'electron'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -19,6 +17,14 @@ async function createWindow() {
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
   })
+  win.setMinimumSize(1100, 770);
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    if (!process.env.IS_TEST) win.webContents.openDevTools()
+  } else {
+    createProtocol('app')
+    win.loadURL('app://./index.html')
+  }
 }
 
 app.on('window-all-closed', () => {
