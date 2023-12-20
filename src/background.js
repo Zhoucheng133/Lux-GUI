0d0ce0aa41f736cea4b75db41a6b9ee798783217
zhoucheng <zhoucheng133@outlook.com>
2023-12-20 10:00:34 +0800

    修改electron配置

--- src/background.js
+++ src/background.js
@@ -1,23 +1,26 @@
+'use strict'
+
 import { app, protocol, BrowserWindow } from 'electron'
+import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-var win;
+let win;
 
 async function createWindow() {
   win = new BrowserWindow({
     width: 1100,
     height: 770,
+    frame: false,
     titleBarStyle: 'hiddenInset',
     webPreferences: {
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
       contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
   })
-  win.setMinimumSize(1100, 770);
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
@@ -28,13 +31,11 @@ async function createWindow() {
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  createWindow()
 })
 
 app.on('ready', async () => {
