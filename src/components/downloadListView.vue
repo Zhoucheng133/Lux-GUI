45356427ef521cd22109d249a0e12690650ebd00
zhoucheng <zhoucheng133@outlook.com>
2023-12-26 10:09:19 +0800

    使用svg代替el-icon

--- src/components/downloadListView.vue
+++ src/components/downloadListView.vue
@@ -44,7 +44,9 @@
       <div class="downloadPath">
         <div class="pathText">保存位置</div>
         <el-input v-model="downloadPath" :readonly=true>
-          <el-button @click="pickDownloadPath" type="primary" slot="append" icon="el-icon-folder-opened"></el-button>
+          <el-button @click="pickDownloadPath" type="primary" slot="append">
+            <svg width="13" height="13" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 9V41L9 21H39.5V15C39.5 13.8954 38.6046 13 37.5 13H24L19 7H6C4.89543 7 4 7.89543 4 9Z" stroke="#9b9b9b" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/><path d="M40 41L44 21H8.8125L4 41H40Z" fill="none" stroke="#9b9b9b" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>
+          </el-button>
         </el-input>
       </div>
       <div class="addViewFoot" slot="footer">
